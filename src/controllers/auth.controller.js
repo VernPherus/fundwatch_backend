f190e36016c083f9840e5953c8e6ba98d6bc8b50
VernPherus <vernpherus@gmail.com>
2025-12-23 10:59:40 +0800

    added checkAuth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,13 @@
+import {prisma} from "../lib/prisma"
 
 /**
- * SIGNUP: 
+ * SIGNUP: Create a user and store into database
  * @param {*} req 
  * @param {*} res 
  */
 export const signup = (req, res) => {
+  const { username, firstname, lastname, email, password } = req.body;
+
   res.send("signup route");
 };
 
@@ -25,3 +28,25 @@ export const login = (req, res) => {
 export const logout = (req, res) => {
   res.send("logout route");
 };
+
+/**
+ * SHOW USERS: Displays all users
+ * @param {*} req 
+ * @param {*} res 
+ */
+export const showUsers = async (req, res) => {
+
+  const allUsers = await prisma.user.findMany();
+  res.status(201).json({
+    allUsers
+  })
+};
+
+/**
+ * CHECK AUTH: Checks if current user is authenticated
+ * @param {*} req 
+ * @param {*} res 
+ */
+export const checkAuth = async (req, res) => {
+  
+}

f5ff25cbb940378ac68f7017360ae90d6475823c
VINCENT <johnvincentjoaquin34@gmail.com>
2026-01-06 09:34:54 +0800

    payee routes

--- src/routes/payee.route.js
+++ src/routes/payee.route.js
@@ -1,9 +1,18 @@
-import express from "express";
+import express from 'express';
+import { 
+    newPayee, 
+    displayPayee, 
+    showPayee, 
+    editPayee,
+    removePayee
+} from '../controllers/payee.controller.js';
 
 const router = express.Router();
 
-/**
- * * PAYEE ROUTES
- */
+router.get('/newpayee', newPayee);
+router.get('/displaypayee', displayPayee);
+router.get('/showpayee/:id', showPayee);
+router.get('/editpayee/:id', editPayee);
+router.get('/removepayee/:id', removePayee);
 
 export default router;

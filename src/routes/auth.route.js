6be00987a483875dbdb508fe79b0887231948f5f
VernPherus <vernpherus@gmail.com>
2026-01-16 10:01:05 +0800

    PATCH: Fixed prisma

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -18,6 +18,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
 router.get("/check", protectRoute, checkAuth);
 
 router.get("/showUsers", protectRoute, authorize(["ADMIN"]), showUsers);
